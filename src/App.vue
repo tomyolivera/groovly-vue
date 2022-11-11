c6b5cfda019a51f8172a4d77349ff984d8d50e89
Mateo <mateoandraca@hotmail.com>
2022-11-10 23:30:55 -0300

    Primera Version
    Splash Screen y Metamask Login

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  <router-view />
+
+  
+
+  <router-view v-slot="{ Component }">
+  <transition name="fade">
+    <component :is="Component" />
+  </transition>
+</router-view>
+<splashScreen v-if="loading" />
 </template>
+
+<script>
+    import SplashScreen from './components/SplashScreen.vue'
+
+    export default {
+        name: "app",
+        data: () => ({
+            loading: true
+        }),
+        mounted() {
+            setTimeout(() => {
+                this.loading = false
+            }, 3000)
+        }
+    }
+</script>
+
