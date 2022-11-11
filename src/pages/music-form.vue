b146140b77cf2201796c752f948d3c66a1ab1198
Mateo <mateoandraca@hotmail.com>
2022-11-11 10:13:23 -0300

    Primera Version Music-Form

--- src/pages/music-form.vue
+++ src/pages/music-form.vue
@@ -1,11 +1,22 @@
 <template lang="">
-    <div>
-        <span>Music form</span>
+    <div class="background">
+        <defaultText text="Let us know you better" style="font-size: 16px;position: absolute;top: 9%;"/>
+        <defaultText text="What Music do you like?" style="font-size: 10px;position: absolute;top: 15%;"/>
+        
+        <ul>
+            <li v-for="item in music">
+                <musicBtn :text="item.text" :icon="icon"/>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            music:[{text:"Pop",icon:"emojione-monotone:microphone"},]
+        }
+    }
 }
 </script>
 <style lang="">
